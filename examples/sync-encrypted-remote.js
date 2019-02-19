58868f0738d862c5b9be9f102c76f9a58506f70e
Paul Sutherland <paul@liquidbinary.co.uk>
2019-02-19 16:56:17 +0000

    Update examples

--- examples/sync-encrypted-remote.js
+++ examples/sync-encrypted-remote.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const PouchDB = require('pouchdb')
 const PouchWebSql = require('pouchdb-adapter-node-websql')
-const SimpleCryptor = require('../src/index') // aka 'simple-cryptor-pouch'
+const SecurePouch = require('../src/index') // aka 'polyonic-secure-pouch'
 
 // use sqlite for simplicity
 const dbPath = path.join(__dirname, '/.pouchdb')
 PouchDB.plugin(PouchWebSql)
-PouchDB.plugin(SimpleCryptor)
+PouchDB.plugin(SecurePouch)
 
 const localPath = dbPath + '/sync-remote.db'
 const remoteURL = 'http://127.0.0.1:5984'
@@ -14,10 +14,10 @@ const remoteURL = 'http://127.0.0.1:5984'
 const local = PouchDB(localPath, {adapter: 'websql'})
 const remote = PouchDB(remoteURL)
 
-remote.simplecryptor('password')
+remote.encrypt('password')
 
 // comment out to encrypt only the remote
-// local.simplecryptor('password')
+// local.encrypt('password')
 
 PouchDB.sync(local, remote, {live: true, retry: true})
   .on('complete', info => console.log({output: info, message: 'complete'}))
